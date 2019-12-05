dbfcaf3cfa175357196c0581b5b01ff3c9cde84c
lishujun5 <624297561@qq.com>
2019-12-05 17:26:34 +0800

    [CHG]delete useless file and change the queue.cpp context

--- src/queue/queue.cpp
+++ src/queue/queue.cpp
@@ -2,34 +2,135 @@
 #include <vector>
 #include <list>
 using namespace std;
+class MyQueue {
+    private:
+        // store elements
+        vector<int> data;       
+        // a pointer to indicate the start position
+        int p_start;            
+    public:
+        MyQueue():p_start{0}{}
+        /** Qinsert an element into the queue. Return true if the operation is successful. */
+        bool enQueue(int x) {
+            data.push_back(x);
+            return true;
+        }
+        /** Delete an element from the queue. Return true if the operation is successful. */
+        bool deQueue() {
+            if (isEmpty()) {
+                return false;
+            }
+            p_start++;
+            return true;
+        };
+        /** Get the Qfront item from the queue. */
+        int Qfront() {
+            return data[p_start];
+        };
+        /** Checks whether the queue is empty or not. */
+        bool isEmpty()  {
+            return p_start >= data.size();
+        }
+};
+class MycycleQueue{
+    private:
+        vector<int> data;
+        const int queueLen;      //é˜Ÿåˆ—é•¿åº¦
+        int queueCount;
+        vector<int>::iterator Qfront;
+        vector<int>::iterator Qinsert;
+    public:
+        MycycleQueue(int len):queueLen(len),queueCount(0){
+            data.resize(5,0);
+            Qfront = data.begin() ; 
+            Qinsert = data.begin();
+            }
+        int Front(void)
+        {
+                return queueCount>0?*Qfront:INT_MAX;
+        }
+        int get_size()
+        {
+            return data.size();
+        }
+        bool enQueue(int val)
+        {
+            if(queueCount >= queueLen)
+            {
+                cerr<<"capacity is enough"<<endl;
+                return false;
+            }
 
-int main() {
-    MyQueue q;
-    q.enQueue(5);
-    q.enQueue(3);
-    vector<int> data{1,2,3,4,5};
-    MycycleQueue *p = new MycycleQueue(5);
-    p->enQueue(8);
-    p->enQueue(9);
-    p->enQueue(10);
-    p->enQueue(11);
-    p->enQueue(12);
-    p->deQueue();
-    p->enQueue(13);
-    p->print_queue();
-    delete p;
-   // p->print_queue();
-    #if 0
-    if (!q.isEmpty()) {
-        cout << q.Qfront() << endl;
-    }
-    q.deQueue();
-    if (!q.isEmpty()) {
-        cout << q.Qfront() << endl;
-    }
-    q.deQueue();
-    if (!q.isEmpty()) {
-        cout << q.Qfront() << endl;
-    }
-    #endif
-}
+            if(Qinsert == data.end()-1)
+            {
+                *Qinsert = val;
+                Qinsert = data.begin();
+            }
+            else
+            {
+                *Qinsert = val;
+                Qinsert++;
+            }
+            queueCount++;
+            return true;
+        }
+        bool deQueue()
+        {
+            if(!queueCount)
+            {
+                return false;
+            }
+            if(Qfront == data.end()-1)
+            {
+                Qfront = data.begin();
+            }
+            else
+            {
+                Qfront++;
+            }
+            queueCount--;
+            return true;
+        }
+        int rear()
+        {
+            if(queueCount<=0)
+            {
+                return INT_MAX;
+            }
+            if(Qinsert == data.begin())
+            {
+                return *data.end();
+            }
+            else
+            {
+                return *(Qinsert-1);
+            }
+        }
+        bool isEmpty()
+        {
+            return queueCount==0?true:false;
+        }
+        bool isFull()
+        {
+            return queueLen == queueCount?true:false;
+        }
+        void print_queue()
+        {
+            int num = queueCount;
+            auto i = Qfront;
+            while(num)
+            {
+                if(i ==  data.end()-1)
+                {
+                    cout<<*i<<endl;
+                    i = data.begin();
+                }
+                else
+                {
+                    cout<<*i<<endl;
+                    i++;
+                }
+                num--;
+            }
+        }
+};

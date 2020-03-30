862a524b734214d55f5a3c6e0a73f24185326e97
lishujun5 <624297561@qq.com>
2020-03-30 23:01:07 +0800

    [CHG]完成平衡二叉树。。。知道什么要发明红黑树了，平衡二叉树，实现起来因为需要考虑到完美平衡，需要在旋转之后更新所有父节点子树高度，时间复杂度最高为log2N，在插入元素时太消耗时间了

--- others/平衡二叉树/blcTree.hpp
+++ others/平衡二叉树/blcTree.hpp
@@ -85,20 +85,21 @@ bool BlcTree<T,N>::LeftCircle(PtNode circleNode)
 /*旋转*/
     hin_CurNode->leftNode = pre_CurNode;
     pre_CurNode->pre = hin_CurNode;
-    pre_CurNode->leftDepth = max(pre_CurNode->rightDepth,pre_CurNode->leftDepth) + 1;
+    hin_CurNode->leftDepth = pre_CurNode->leftDepth + 1;
 
-    if(fatherNode)  //需要旋转点为根节点
+    if(fatherNode)
     {
         if(fatherNode -> rightNode == pre_CurNode)
         {
             fatherNode->rightNode = hin_CurNode;
-            fatherNode->rightDepth = max(hin_CurNode->rightDepth,hin_CurNode->leftDepth) + 1;
+            fatherNode->rightDepth = max(hin_CurNode->rightDepth , hin_CurNode->leftDepth)+1;
         }
         else
         {
              fatherNode->leftNode = hin_CurNode;
-             fatherNode->leftDepth = max(hin_CurNode->rightDepth,hin_CurNode->leftDepth) + 1;
+             fatherNode->leftDepth = max(hin_CurNode->rightDepth , hin_CurNode->leftDepth)+1;
         }
+        
         hin_CurNode->pre = fatherNode;
     }
     else
@@ -106,7 +107,28 @@ bool BlcTree<T,N>::LeftCircle(PtNode circleNode)
         this->root = hin_CurNode;
         hin_CurNode->pre = nullptr;
     }
-
+    while(fatherNode && fatherNode->pre)  //在插入节点的子树上的父节点，都需要更新一下左右树高度，否则在大量插入的情况下会产生问题
+    {
+        if(fatherNode ->pre-> rightNode == fatherNode)
+        {
+            int preDepth = fatherNode->pre->rightDepth;
+            fatherNode->pre->rightDepth = max(fatherNode->rightDepth , fatherNode->leftDepth)+1;
+            if(preDepth == fatherNode->pre->rightDepth) //当某一处父节点保持不变时，则不需要向上遍历他的父节点，节省时间
+            {
+                break;
+            }
+        }
+        else
+        {
+            int preDepth = fatherNode->pre->leftDepth;
+            fatherNode->pre->leftDepth = max(fatherNode->rightDepth , fatherNode->leftDepth)+1;
+            if(preDepth == fatherNode->pre->leftDepth) //当某一处父节点保持不变时，则不需要向上遍历他的父节点，节省时间
+            {
+                break;
+            }
+        }
+        fatherNode = fatherNode->pre;
+    }
     return true;
 }
 template <class T ,class N>
@@ -130,7 +152,7 @@ bool BlcTree<T,N>::RightCirle(PtNode circleNode)
 /*旋转*/
     hin_CurNode->rightNode = pre_CurNode;
     pre_CurNode->pre = hin_CurNode;
-    hin_CurNode->rightDepth = max(pre_CurNode->rightDepth,pre_CurNode->leftDepth) + 1;
+    hin_CurNode->rightDepth = pre_CurNode->rightDepth + 1;
 
     if(fatherNode)  //需要旋转点为根节点
     {
@@ -151,7 +173,18 @@ bool BlcTree<T,N>::RightCirle(PtNode circleNode)
         this->root = hin_CurNode;
         hin_CurNode->pre = nullptr;
     }
-
+    while(fatherNode && fatherNode->pre)
+    {
+        if(fatherNode ->pre-> rightNode == fatherNode)
+        {
+            fatherNode->pre->rightDepth = max(fatherNode->rightDepth , fatherNode->leftDepth)+1;
+        }
+        else
+        {
+            fatherNode->pre->leftDepth = max(fatherNode->rightDepth , fatherNode->leftDepth)+1;
+        }
+        fatherNode = fatherNode->pre;
+    }
     return true;
 }
 template <class T ,class N>
@@ -188,22 +221,34 @@ bool BlcTree<T,N>::insertNode(PtNode tmpNode)
     cout<<endl;
     cout<<endl;
     cout<<endl;
-    
+    cout<<"afterInsert***********"<<endl;
+    Order_type tmp = this->preOrder();
+    for(auto i:tmp)
+    {
+        cout<<"i->val = "<<i->val<<"----->"<<"leftDepth = "<<i->leftDepth<<"-------->"<<"rightDepth = "<<i->rightDepth<<endl;
+    }
+    cout<<"***********afterInsert"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
     if(nullptr != (needCircle = isBalance(tmpNode , &circleType)))
     {
+        cout<<"needCircle->val = "<<needCircle->val<<endl;
+        cout<<"circleType = "<<circleType<<endl;
+        circleType = circleType & 0x3;
         if(circleType == 0)
         {
             cout<<"LeftCircle"<<endl;
             LeftCircle(needCircle);
         }
-        else if(circleType == 1)
-        {
-            cout<<"LRCircle"<<endl;
-            RLCircle(needCircle);
-        }
         else if(circleType == 2)
         {
             cout<<"RLCircle"<<endl;
+            RLCircle(needCircle);
+        }
+        else if(circleType == 1)
+        {
+            cout<<"LRCircle"<<endl;
             LRCircle(needCircle);
         }
         else if(circleType == 3)
@@ -217,7 +262,13 @@ bool BlcTree<T,N>::insertNode(PtNode tmpNode)
             return false;
         }
     }
-    
+    cout<<"afterCircle***********"<<endl;
+    Order_type tmph = this->preOrder();
+    for(auto i:tmph)
+    {
+        cout<<"i->val = "<<i->val<<"----->"<<"leftDepth = "<<i->leftDepth<<"-------->"<<"rightDepth = "<<i->rightDepth<<endl;
+    }
+    cout<<"***********afterCircle"<<endl;
     return true;
 }
 template <class T ,class N>
@@ -242,7 +293,7 @@ bool BlcTree<T,N>::insertNode(PtNode tmpNode , PtNode curNode)
             ret = insertNode(tmpNode,curNode->leftNode);
             if(ret == true)
             {
-                curNode->leftDepth++;
+                curNode->leftDepth = max(curNode->leftNode->leftDepth,curNode->leftNode->rightDepth)+1;
             }
         }
     }
@@ -260,7 +311,7 @@ bool BlcTree<T,N>::insertNode(PtNode tmpNode , PtNode curNode)
             ret = insertNode(tmpNode,curNode->rightNode);
             if(ret == true)
             {
-                curNode->rightDepth++;
+                curNode->rightDepth = max(curNode->rightNode->rightDepth , curNode->rightNode->leftDepth)+1;
             }
         }
     }
@@ -301,10 +352,10 @@ void BlcTree<T,N>::setFatherNode()
 }
 /***********************
 circleType:
-0x00  ----->    右右
-0x01  ----->    右左
-0x10  ----->    左右
-0x11  ----->    左左
+00  ----->    右右
+01  ----->    左右
+10  ----->    右左
+11  ----->    左左
 return:nullptr --- > 无不平衡节点
 ***********************/
 template <class T ,class N>
@@ -327,22 +378,10 @@ typename BlcTree<T,N>::PtNode BlcTree<T,N>::isBalance(PtNode insertNode , int* c
         }
         else
         {
-            if(isEnough)
+            *circleType = *circleType<<1;
+            if(tmpNode->pre && tmpNode->pre->leftNode == tmpNode)
             {
-                *circleType = *circleType>>1;
-            }
-            else
-            {
-                recodeTime++;
-                if(recodeTime == 2)
-                {
-                    isEnough = true;
-                }
-            }
-            
-            if(tmpNode->pre->leftNode == tmpNode)
-            {
-                    *circleType = *circleType | 0x3;
+                *circleType = *circleType | 0x1;
             }
             tmpNode = tmpNode->pre;
         }
--- others/平衡二叉树/mainblc.cpp
+++ others/平衡二叉树/mainblc.cpp
@@ -3,9 +3,17 @@
 int main()
 {
     BlcTree<int> dd;
-    for (int i = 0; i < 20; i++)
+    #if 0
+    dd.insertNode(3);
+    dd.insertNode(6);
+    dd.insertNode(17);
+    dd.insertNode(15);
+    dd.insertNode(13);
+    dd.insertNode(12);
+    #endif
+    for (int i = 0; i < 100; i++)
     {
-        dd.insertNode(rand()%20);
+        dd.insertNode(rand()%100);
     }
     dd.display_Tree();
     return 0;
